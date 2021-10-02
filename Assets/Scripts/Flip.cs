cc303793198ffd96df7d3cd9ba1994bf08204f77
donatas <vizitas7@gmail.com>
2021-10-02 23:16:24 +0300

    biker animation

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public float speed = 100;
+    void Start()
+    {
+        InvokeRepeating("flip", 0, speed);
+    }
+
+    public void flip()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
