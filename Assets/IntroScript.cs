01a8fcb79d65203b1d92c1b341e32d0a74d21501
Vilmantas Petrusevičius <e>
2021-10-04 00:21:12 +0300

    Intro scene improvements

--- Assets/IntroScript.cs
+++ Assets/IntroScript.cs
@@ -23,7 +23,7 @@ public class IntroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeSinceLevelLoad < 0.2f) return;
+        if (Time.timeSinceLevelLoad < 0.7f) return;
 
         var t = Time.timeSinceLevelLoad % 60f / 60f;
 
@@ -31,7 +31,7 @@ public class IntroScript : MonoBehaviour
 
         Sun.rotation = Quaternion.Euler(new Vector3(Sun.rotation.eulerAngles.x, t * 90, Sun.rotation.eulerAngles.z));
 
-        var alpha = Mathf.Lerp(1f, 0f, (Time.timeSinceLevelLoad - 0.2f) / FadeInTime );
+        var alpha = Mathf.Lerp(1f, 0f, (Time.timeSinceLevelLoad - 0.7f) / FadeInTime );
 
         Cover.color = new Color(Cover.color.r, Cover.color.g, Cover.color.b, alpha);
 
