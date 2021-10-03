15e101703ef4c59445a788560ca4b31cf4deba13
Vilmantas Petrusevičius <e>
2021-10-04 00:09:58 +0300

    Intro scene improvements

--- Assets/IntroScript.cs
+++ Assets/IntroScript.cs
@@ -12,6 +12,8 @@ public class IntroScript : MonoBehaviour
 
     public GameObject Canvas;
 
+    public Transform Sun;
+
     private void Start()
     {
         Canvas.SetActive(true);
@@ -21,9 +23,15 @@ public class IntroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeSinceLevelLoad < 2f) return;
+        if (Time.timeSinceLevelLoad < 0.2f) return;
 
-        var alpha = Mathf.Lerp(1f, 0f, (Time.timeSinceLevelLoad - 2) / FadeInTime );
+        var t = Time.timeSinceLevelLoad % 60f / 60f;
+
+        Debug.Log(t);
+
+        Sun.rotation = Quaternion.Euler(new Vector3(Sun.rotation.eulerAngles.x, t * 90, Sun.rotation.eulerAngles.z));
+
+        var alpha = Mathf.Lerp(1f, 0f, (Time.timeSinceLevelLoad - 0.2f) / FadeInTime );
 
         Cover.color = new Color(Cover.color.r, Cover.color.g, Cover.color.b, alpha);
 
